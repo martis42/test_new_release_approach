cf28244c218120db0647e400b2094d615ab7f76c
Martin Medler <36563496+martis42@users.noreply.github.com>
2025-07-23 18:39:07 +0200

    use python helper

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -2,20 +2,22 @@
 
 set -o errexit -o nounset -o pipefail
 
-# Set by GH actions, see
-# https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
-readonly TAG=$1
-# The prefix is chosen to match what GitHub generates for source archives.
-# This guarantees that users can easily switch from a released artifact to a source archive
-# with minimal differences in their code (e.g. strip_prefix remains the same)
-readonly PREFIX="depend_on_what_you_use-${TAG}"
-readonly ARCHIVE="${PREFIX}.tar.gz"
+# # Set by GH actions, see
+# # https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
+# readonly TAG=$1
+# # The prefix is chosen to match what GitHub generates for source archives.
+# # This guarantees that users can easily switch from a released artifact to a source archive
+# # with minimal differences in their code (e.g. strip_prefix remains the same)
+# readonly PREFIX="depend_on_what_you_use-${TAG}"
+# readonly ARCHIVE="${PREFIX}.tar.gz"
 
-# NB: configuration for 'git archive' is in /.gitattributes
-git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+# # NB: configuration for 'git archive' is in /.gitattributes
+# git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
 
-cat << EOF
-## Test
+# cat << EOF
+# ## Test
 
-foo
-EOF
+# foo
+# EOF
+
+./prepare_release.py --tag $1
